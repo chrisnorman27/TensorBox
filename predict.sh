4776896ffa865752118ee0a774fb9d781fe701a6
Chris Norman <chrisnorman140@gmail.com>
2017-11-05 12:53:53 +0800

    update to use a cpkt model file that has been trained for more iterations

--- predict.sh
+++ predict.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in data/cda/ctx_crops/*.png; do
-  python predict.py $file output/overfeat_rezoom_cda_2017_05_04_04.17/save.ckpt-20000 hypes/overfeat_rezoom_cda.json;
+  python predict.py $file output/overfeat_resnet_rezoom_cda_2017_05_04_06.43/save.ckpt-250000 hypes/overfeat_resnet_rezoom_cda.json;
 done
